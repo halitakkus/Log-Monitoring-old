c985606d0a57f505cf836cedb14f3655728c2033
halit.akkus <halit.akkus-a@tubitak.gov.tr>
2023-04-13 11:41:57 +0300

    dosyalar eklendi.

---
+++ LogMonitoring.MVC/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LogMonitoring.MVC.Models;
+
+namespace LogMonitoring.MVC.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Index(string token)
+    {
+        if (string.IsNullOrEmpty(token))
+        {
+            return View("Error");
+        }
+        
+        return View();
+    }
+    
+     
+}
---
+++ LogMonitoring.MVC/Models/TokenInformation.cs
@@ -0,0 +1,9 @@
+﻿namespace LogMonitoring.MVC.Models;
+
+public class TokenInformation
+{
+    /// <summary>
+    /// Generated token.
+    /// </summary>
+    public string Token { get; set; }
+}
